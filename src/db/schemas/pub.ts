b958f1e1b69d9a57f7f39603adf380eca92be083
CesarWatrin <cesar.watrin@hotmail.com>
2020-12-10 09:14:48 +0100

    barathons models

--- src/db/schemas/pub.ts
+++ src/db/schemas/pub.ts
@@ -1,12 +1,6 @@
 import { Schema } from 'mongoose';
 import { ICustomSchema } from '../../types/database';
-
-const Comment: Schema = new Schema ({
-   text: { type:String, required:true },
-   author: { type:String, required:true },
-   date: { type:String, required:true },
-   rating: { type:Number, required:true }
-});
+import Comment from './comment';
 
 const pubSchema:ICustomSchema = {
    collection: 'pubs',
